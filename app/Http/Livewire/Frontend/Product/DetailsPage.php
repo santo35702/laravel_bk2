eb604b5df837069220b1affda3a50df0950e2172
MD: Suvo <santo35702@gmail.com>
2021-12-31 15:54:15 +0600

    Shop: Products Page created by Livewire

---
+++ app/Http/Livewire/Frontend/Product/DetailsPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Frontend\Product;
+
+use Livewire\Component;
+
+class DetailsPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.product.details-page');
+    }
+}
---
+++ app/Http/Livewire/Frontend/Product/IndexPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Frontend\Product;
+
+use Livewire\Component;
+
+class IndexPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.product.index-page');
+    }
+}
