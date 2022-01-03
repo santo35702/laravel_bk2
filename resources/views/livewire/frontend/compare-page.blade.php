92cb83ed5aff623b4d04d20ce7e5cde3633f7204
MD: Suvo <santo35702@gmail.com>
2022-01-03 12:12:23 +0600

    Shop: Compare Page create, Layout and Content set

---
+++ resources/views/livewire/frontend/compare-page.blade.php
@@ -0,0 +1,131 @@
+<div id="page-content">
+    <!--Page Title-->
+    <div class="page section-header text-center">
+        <div class="page-title">
+            <div class="wrapper"><h1 class="page-width">Compare Product variant1</h1></div>
+        </div>
+    </div>
+    <!--End Page Title-->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 main-col">
+                <div class="compare-page">
+                    <div class="table-wrapper table-responsive">
+                        <table class="table">
+                            <thead>
+                            <tr class="th-compare">
+                                <th valign="middle">Action</th>
+                                <td valign="middle" class="item-row">
+                                  <button type="button" class="remove-compare"><i class="anm anm-times-l" aria-hidden="true"></i></button>
+                                </td>
+                                <td class="item-row">
+                                  <button type="button" class="remove-compare"><i class="anm anm-times-l" aria-hidden="true"></i></button>
+                                </td>
+                                <td class="item-row">
+                                    <button type="button" class="remove-compare"><i class="anm anm-times-l" aria-hidden="true"></i></button>
+                                </td>
+                                <td class="item-row">
+                                    <button type="button" class="remove-compare"><i class="anm anm-times-l" aria-hidden="true"></i></button>
+                                </td>
+                                <td class="item-row">
+                                    <button type="button" class="remove-compare"><i class="anm anm-times-l" aria-hidden="true"></i></button>
+                                </td>
+                            </tr>
+                            </thead>
+                            <tbody id="table-compare">
+                            <tr>
+                                <th valign="middle" class="product-name">Product Name</th>
+                                  <td valign="middle" class="grid-link__title">Edna Dress</td>
+                                  <td class="grid-link__title">Elastic Waist Dress</td>
+                                <td class="grid-link__title">3/4 Sleeve Kimono Dress</td>
+                                <td class="grid-link__title">Cape Dress</td>
+                                <td class="grid-link__title">Cape Dress</td>
+                            </tr>
+                            <tr>
+                                <th valign="middle" class="product-name">Product Image</th>
+                                <td valign="middle" class="item-row"><img src="assets/images/product-images/product-image16.jpg" alt="" class="featured-image">
+                                <div class="product-price product_price"><span>$89,00</span></div>
+                                    <form class="variants clearfix">
+                                        <input type="hidden">
+                                        <button title="Add to Cart" class="add-to-cart btn btn-solid">Add to Cart</button>
+                                    </form>
+                                    <p class="grid-link__title hidden">Cut Dress</p>
+                                </td>
+                                <td class="item-row"><img src="assets/images/product-images/product-image18.jpg" alt="" class="featured-image">
+                                <div class="product-price product_price"><span>$89,00</span></div>
+                                    <form class="variants clearfix">
+                                        <input type="hidden">
+                                        <button title="Add to Cart" class="add-to-cart btn btn-solid">Add to Cart</button>
+                                    </form>
+                                    <p class="grid-link__title hidden">Cut Dress</p>
+                                </td>
+                                <td class="item-row"><img src="assets/images/product-images/product-image20.jpg" alt="" class="featured-image">
+                                    <div class="product-price product_price"><span>$89,00</span></div>
+                                    <form class="variants clearfix">
+                                        <input type="hidden">
+                                        <button title="Add to Cart" class="add-to-cart btn btn-solid">Add to Cart</button>
+                                    </form>
+                                    <p class="grid-link__title hidden">Cut Dress</p>
+                                </td>
+                                <td class="item-row"><img src="assets/images/product-images/product-image23.jpg" alt="" class="featured-image">
+                                    <div class="product-price product_price"><span>$89,00</span></div>
+                                    <form class="variants clearfix">
+                                        <input type="hidden">
+                                        <button title="Add to Cart" class="add-to-cart btn btn-solid">Add to Cart</button>
+                                    </form>
+                                    <p class="grid-link__title hidden">Cut Dress</p>
+                                </td>
+                                <td class="item-row"><img src="assets/images/product-images/product-image28.jpg" alt="" class="featured-image">
+                                    <div class="product-price product_price"><span>$89,00</span></div>
+                                    <form class="variants clearfix">
+                                        <input type="hidden">
+                                        <button title="Add to Cart" class="add-to-cart btn btn-solid">Add to Cart</button>
+                                    </form>
+                                    <p class="grid-link__title hidden">Cut Dress</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th valign="middle" class="product-name">Product Description</th>
+                                <td valign="middle" class="item-row">
+                                  <p class="description-compare">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                                </td>
+                                <td class="item-row">
+                                  <p class="description-compare">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                                </td>
+                                <td class="item-row">
+                                    <p class="description-compare">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration.</p>
+                                </td>
+                                <td class="item-row">
+                                    <p class="description-compare">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</p>
+                                </td>
+                                <td class="item-row">
+                                    <p class="description-compare">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <th valign="middle" class="product-name">Availability</th>
+                                <td valign="middle" class="available-stock">
+                                  <p>In stock</p>
+                                </td>
+                                <td class="available-stock">
+                                  <p>In stock</p>
+                                </td>
+                                <td class="available-stock">
+                                    <p>In stock</p>
+                                </td>
+                                <td class="available-stock">
+                                    <p>In stock</p>
+                                </td>
+                                <td class="available-stock">
+                                    <p>In stock</p>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
