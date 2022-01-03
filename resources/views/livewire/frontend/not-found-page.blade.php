c4f57b99212d9512eee98a93afaf4b099343d6a5
MD: Suvo <santo35702@gmail.com>
2022-01-03 12:47:05 +0600

    Shop: content set in 404 page

--- resources/views/livewire/frontend/not-found-page.blade.php
+++ resources/views/livewire/frontend/not-found-page.blade.php
@@ -1,3 +1,15 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+<div id="page-content">
+    <!-- Lookbook Start -->
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="empty-page-content text-center">
+                    <h1>404 Page Not Found</h1>
+                    <p>The page you requested does not exist.</p>
+                    <p><a href="{{ route('products.index') }}" class="btn btn--has-icon-after">Continue shopping <i class="fa fa-caret-right" aria-hidden="true"></i></a></p>
+                  </div>
+            </div>
+        </div>
+    </div>
+    <!-- Lookbook Start -->
 </div>
