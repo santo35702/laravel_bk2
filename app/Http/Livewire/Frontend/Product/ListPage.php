8644a41caa2d95fa07b83ea48489c15dea42974f
MD: Suvo <santo35702@gmail.com>
2022-01-04 16:33:05 +0600

    Shop: Shop Layout set in list page

--- app/Http/Livewire/Frontend/Product/ListPage.php
+++ app/Http/Livewire/Frontend/Product/ListPage.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire\Frontend\Product;
 
 use Livewire\Component;
+use App\Models\Product;
 
 class ListPage extends Component
 {
     public function render()
     {
-        return view('livewire.frontend.product.list-page');
+        $products = Product::paginate(20);
+        $popular_products = Product::inRandomOrder()->limit(5)->get();
+        return view('livewire.frontend.product.list-page', ['products' => $products, 'popular_products' => $popular_products])->layout('layouts.base');
     }
 }
