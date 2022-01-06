99f19fe1241603c2db8901732ff7fdbdf66e5f73
MD: Suvo <santo35702@gmail.com>
2022-01-06 17:29:33 +0600

    Shop: Authentication type and some style change

--- vendor/laravel/jetstream/resources/views/components/validation-errors.blade.php
+++ vendor/laravel/jetstream/resources/views/components/validation-errors.blade.php
@@ -1,11 +1,16 @@
 @if ($errors->any())
     <div {{ $attributes }}>
+        {{-- <h4 class="alert-heading font-medium">{{ __('Whoops! Something went wrong.') }}</h4> --}}
         <div class="font-medium text-red-600">{{ __('Whoops! Something went wrong.') }}</div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+        {{-- <hr> --}}
+        <ul class="mt-3 list-disc list-inside text-sm text-red-600 nav">
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <li class="mb-0"><i class="fas fa-arrow-right"></i> {{ $error }}</li>
             @endforeach
         </ul>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
     </div>
 @endif
