e922458296bd6cf37e8d764de3b8cebf563cb67f
MD: Suvo <santo35702@gmail.com>
2022-01-06 14:47:58 +0600

    Shop: Product List by Category Component created

---
+++ app/Http/Livewire/Frontend/Product/ListByCategoryPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Frontend\Product;
+
+use Livewire\Component;
+
+class ListByCategoryPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.frontend.product.list-by-category-page');
+    }
+}
