88a5e304fdb96ddb16f2ca54098c2ad4c157439c
MD: Suvo <santo35702@gmail.com>
2022-01-06 18:11:55 +0600

    Admin: Admin Route redirect from User route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/';
-    public const ADMINHOME = '/';
+    public const ADMINHOME = '/admin/dashboard';
 
     /**
      * The controller namespace for the application.
