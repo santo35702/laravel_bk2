f73479b5d2605d451e2bac5bafeee1186536e986
MD: Suvo <santo35702@gmail.com>
2022-01-07 15:41:29 +0600

    Admin: Category delete function works

--- app/Http/Livewire/Admin/Category/IndexPage.php
+++ app/Http/Livewire/Admin/Category/IndexPage.php
@@ -4,9 +4,18 @@ namespace App\Http\Livewire\Admin\Category;
 
 use Livewire\Component;
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class IndexPage extends Component
 {
+    public function deleteItem(Request $request, $id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        $request->session()->flash('status', 'Category has been Deleted successfully!');
+        return redirect()->route('admin.categories.index');
+    }
+
     public function render()
     {
         $categories = Category::get();
