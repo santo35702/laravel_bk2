b641c176f4f6f1d0f03934d6039466319f06c670
MD: Suvo <santo35702@gmail.com>
2022-01-07 14:48:04 +0600

    Admin: Category Edit form & Submit function works

--- app/Http/Livewire/Admin/Category/EditCategoryPage.php
+++ app/Http/Livewire/Admin/Category/EditCategoryPage.php
@@ -3,11 +3,57 @@
 namespace App\Http\Livewire\Admin\Category;
 
 use Livewire\Component;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Models\Category;
 
 class EditCategoryPage extends Component
 {
+    public $name;
+    public $slug;
+    public $description;
+
+    public function mount($id)
+    {
+        $category = Category::where('id', $id)->first();
+        $this->category_id = $category->id;
+        $this->name = $category->name;
+        $this->slug = $category->slug;
+        $this->description = $category->description;
+    }
+
+    public function generateSlug()
+    {
+        $this->slug = Str::slug($this->name);
+    }
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'name' => 'required|max:255',
+            'slug' => 'required|unique:categories|max:255',
+            'description' => 'required',
+        ]);
+    }
+
+    function updateItem(Request $request)
+    {
+        $this->validate([
+            'name' => 'required|max:255',
+            'slug' => 'required|unique:categories|max:255',
+            'description' => 'required',
+        ]);
+
+        $category = Category::find($this->category_id);
+        $category->name = $this->name;
+        $category->slug = $this->slug;
+        $category->description = $this->description;
+        $category->save();
+        $request->session()->flash('status', 'Category has been updated successfully!');
+    }
+
     public function render()
     {
-        return view('livewire.admin.category.edit-category-page');
+        return view('livewire.admin.category.edit-category-page')->layout('layouts.admin');
     }
 }
--- resources/views/livewire/admin/category/edit-category-page.blade.php
+++ resources/views/livewire/admin/category/edit-category-page.blade.php
@@ -1,3 +1,113 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+<div class="content-wrapper">
+
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset('admin/plugins/summernote/summernote-bs4.min.css') }}">
+
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Edit Categories</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('admin.categories.index') }}">Categories</a></li>
+                        <li class="breadcrumb-item active">Edit Categories</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    @if (session('status'))
+                        <div class="alert alert-success text-uppercase" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form wire:submit.prevent="updateItem">
+                        <div class="card card-primary card-outline">
+                            <div class="card-header">
+                                <h3 class="card-title">Update Information <small class="ml-2"><b class="badge badge-danger mr-1">Note:</b> All <span class="text-danger fa-2x">*</span> marks must fill required.</small></h3>
+                                <div class="card-tools">
+                                    <a href="{{ route('admin.categories.index') }}" class="btn btn-sm btn-info"><i class="fas fa-undo"></i></a>
+                                </div>
+                                <!-- /.card-tools -->
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                @error ('slug')
+                                    <div class="alert alert-danger">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+
+                                <div class="form-group">
+                                    <label for="name">Name <span class="text-danger">*</span></label>
+                                    <input type="text" id="name" class="form-control @error('name') is-invalid @enderror" placeholder="Enter Category Name" wire:model="name" wire:keyup="generateSlug">
+                                    @error ('name')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input type="hidden" class="form-control" wire:model="slug">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Description <span class="text-danger">*</span></label>
+                                    <textarea class="form-control @error('description') is-invalid @enderror summernote" id="description" wire:model="description"></textarea>
+                                    @error ('description')
+                                        <div class="invalid-feedback">
+                                            {{ $message }}
+                                        </div>
+                                    @enderror
+                                </div>
+                            </div>
+                            <!-- /.card-body -->
+                        </div>
+                        <!-- /.card -->
+                        <div class="form-group">
+                            <a href="{{ route('admin.categories.index') }}" class="btn btn-secondary">Cancel</a>
+                            <button type="submit" class="btn btn-sm btn-success float-right">Save Changes</button>
+                        </div>
+                    </form>
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
 </div>
+
+@push('script')
+    <!-- AdminLTE for demo purposes -->
+    <script src="{{ asset('admin/dist/js/demo.js') }}"></script>
+    <!-- Summernote -->
+    <script src="{{ asset('admin/plugins/summernote/summernote-bs4.min.js') }}"></script>
+@endpush
+
+@push('script1')
+    <!-- Page specific script -->
+    <script>
+      $(function () {
+        // Summernote
+        $('.summernote').summernote({
+            placeholder: 'Enter Description',
+            tabsize: 2,
+            height: 150,   //set editable area's height
+            // fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Merriweather'],
+            // fontNamesIgnoreCheck: ['Merriweather'],
+            // lineHeights: ['0.2', '0.3', '0.4', '0.5', '0.6', '0.8', '1.0', '1.2', '1.4', '1.5', '2.0', '3.0']
+        });
+      })
+    </script>
+@endpush
