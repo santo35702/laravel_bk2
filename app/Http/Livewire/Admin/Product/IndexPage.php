4bcfb5c39284d5d9a9127e0abf31638d8ed99e12
MD: Suvo <santo35702@gmail.com>
2022-01-09 12:07:27 +0600

    Admin: Product delete done

--- app/Http/Livewire/Admin/Product/IndexPage.php
+++ app/Http/Livewire/Admin/Product/IndexPage.php
@@ -4,9 +4,18 @@ namespace App\Http\Livewire\Admin\Product;
 
 use Livewire\Component;
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class IndexPage extends Component
 {
+    public function deleteItem(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        $request->session()->flash('status', 'Product has been Deleted successfully!');
+        return redirect()->route('admin.products.index');
+    }
+
     public function render()
     {
         $products = Product::get();
