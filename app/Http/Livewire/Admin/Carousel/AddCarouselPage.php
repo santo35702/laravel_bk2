b5514918bcef729365ed79f92b594ba657621a5c
MD: Suvo <santo35702@gmail.com>
2022-01-09 14:36:22 +0600

    Admin: Home Carousel Form & Add Function add successfully

--- app/Http/Livewire/Admin/Carousel/AddCarouselPage.php
+++ app/Http/Livewire/Admin/Carousel/AddCarouselPage.php
@@ -3,9 +3,59 @@
 namespace App\Http\Livewire\Admin\Carousel;
 
 use Livewire\Component;
+use Illuminate\Support\Carbon;
+use Illuminate\Http\Request;
+use Livewire\WithFileUploads;
+use App\Models\HomeCarousel;
 
 class AddCarouselPage extends Component
 {
+    use WithFileUploads;
+    
+    public $title;
+    public $subtitle;
+    public $link;
+    public $image;
+    public $status;
+
+    public function mount()
+    {
+        $this->status = 0;
+    }
+
+    public function updated($fields)
+    {
+        $this->validateOnly($fields, [
+            'title' => 'required|max:255',
+            'subtitle' => 'required|max:255',
+            'link' => 'required|max:255',
+            'image' => 'required|mimes:jpeg,jpg,png',
+            'status' => 'required',
+        ]);
+    }
+
+    public function storeItem(Request $request)
+    {
+        $this->validate([
+            'title' => 'required|max:255',
+            'subtitle' => 'required|max:255',
+            'link' => 'required|max:255',
+            'image' => 'required|mimes:jpeg,jpg,png',
+            'status' => 'required',
+        ]);
+
+        $carousel = new HomeCarousel();
+        $carousel->title = $this->title;
+        $carousel->subtitle = $this->subtitle;
+        $carousel->link = $this->link;
+        $imageName = Carbon::now()->timestamp . '.' . $this->image->extension();
+        $this->image->storeAs('slideshow-banners', $imageName);
+        $carousel->image = $imageName;
+        $carousel->status = $this->status;
+        $carousel->save();
+        $request->session()->flash('status', 'New Carousel Created successfully!');
+    }
+
     public function render()
     {
         return view('livewire.admin.carousel.add-carousel-page')->layout('layouts.admin');
