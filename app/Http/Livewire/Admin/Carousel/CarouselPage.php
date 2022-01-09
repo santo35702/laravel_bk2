8646acac32ac22fa831d8174968d57631fc9bb70
MD: Suvo <santo35702@gmail.com>
2022-01-09 16:31:12 +0600

    Admin: Carousel delete function works fine

--- app/Http/Livewire/Admin/Carousel/CarouselPage.php
+++ app/Http/Livewire/Admin/Carousel/CarouselPage.php
@@ -4,9 +4,18 @@ namespace App\Http\Livewire\Admin\Carousel;
 
 use Livewire\Component;
 use App\Models\HomeCarousel;
+use Illuminate\Http\Request;
 
 class CarouselPage extends Component
 {
+    public function deleteItem(Request $request, $id)
+    {
+        $carousel = HomeCarousel::find($id);
+        $carousel->delete();
+        $request->session()->flash('status', 'Carousel has been Deleted successfully!');
+        return redirect()->route('admin.carousel.index');
+    }
+    
     public function render()
     {
         $carousel = HomeCarousel::get();
