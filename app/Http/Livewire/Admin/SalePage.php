3b89c833836026ecc26aef44d2c6b62aaf80f14e
MD: Suvo <santo35702@gmail.com>
2022-01-10 16:04:44 +0600

    Admin: Sale Livewire created & content set

--- app/Http/Livewire/Admin/SalePage.php
+++ app/Http/Livewire/Admin/SalePage.php
@@ -17,7 +17,16 @@ class SalePage extends Component
         $this->sale_date = $sale->sale_date;
         $this->status = $sale->status;
     }
-    
+
+    public function updateSale(Request $request)
+    {
+        $sale = Sale::find(1);
+        $sale->sale_date = $this->sale_date;
+        $sale->status = $this->status;
+        $sale->save();
+        $request->session()->flash('status', 'Sale Time Updated successfully!');
+    }
+
     public function render()
     {
         return view('livewire.admin.sale-page')->layout('layouts.admin');
