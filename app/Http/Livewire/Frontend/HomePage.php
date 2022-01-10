2fd5a91dcda85aafea1d34fa695a53a174e0157c
MD: Suvo <santo35702@gmail.com>
2022-01-10 13:14:08 +0600

    Shop: New Arrival Category & Product view in Home page

--- app/Http/Livewire/Frontend/HomePage.php
+++ app/Http/Livewire/Frontend/HomePage.php
@@ -3,13 +3,29 @@
 namespace App\Http\Livewire\Frontend;
 
 use Livewire\Component;
+use App\Models\Product;
+use App\Models\Category;
 use App\Models\HomeCarousel;
+use App\Models\HomeCategory;
+use Illuminate\Http\Request;
+use Cart;
 
 class HomePage extends Component
 {
+    public function AddToCart(Request $request, $id, $title, $price)
+    {
+        Cart::instance('cart')->add($id, $title, 1, $price)->associate('App\Models\Product');
+        $request->session()->flash('status', 'Product Add to Cart successful!');
+        return redirect()->route('cart');
+    }
+
     public function render()
     {
         $carousel = HomeCarousel::where('status', 1)->inRandomOrder()->limit(3)->get();
-        return view('livewire.frontend.home-page', ['carousel' => $carousel])->layout('layouts.base');
+        $new_arrival_cat = HomeCategory::find(1);
+        $new_arrival_cat_id = explode(',', $new_arrival_cat->category_id);
+        $no_of_products = $new_arrival_cat->no_of_products;
+        $new_arrival = Category::whereIn('id', $new_arrival_cat_id)->get();
+        return view('livewire.frontend.home-page', ['carousel' => $carousel, 'new_arrival' => $new_arrival, 'no_of_products' => $no_of_products])->layout('layouts.base');
     }
 }
